f81a8290744759c319c7f17580c379f811cadc0e
Sajid <shojolislamshajid55@gmail.com>
2022-11-18 01:31:34 +0600

    Logout

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -4276,9 +4276,15 @@ License: For each use you must have a valid license purchased only from above li
 											</div>
 											<!--end::Menu item-->
 											<!--begin::Menu item-->
-											<div class="menu-item px-5">
-												<a href="../../demo1/dist/authentication/flows/basic/sign-in.html" class="menu-link px-5">Sign Out</a>
-											</div>
+                                            {{-- <div class="menu-item px-5">
+                                                <a href="../../demo1/dist/authentication/flows/basic/sign-in.html" class="menu-link px-5">Sign Out</a>
+                                            </div> --}}
+                                            <form method="POST" action="{{ route('logout') }}">
+                                                @csrf
+                                                <div class="menu-item px-5">
+                                                    <a href="route('logout')" onclick="event.preventDefault(); this.closest('form').submit();" class="menu-link px-5">Sign Out</a>
+                                                </div>
+                                            </form>
 											<!--end::Menu item-->
 											<!--begin::Menu separator-->
 											<div class="separator my-2"></div>
