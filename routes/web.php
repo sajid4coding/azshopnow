91e8570332231289d89992a6cb4a0964e827e623
Md. Shrabon Hossain <90843040+shrabon-dev@users.noreply.github.com>
2022-11-20 03:04:46 +0600

    web.php conflict solve

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
-
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\{ProfileController, CategoryController};
+use App\Http\Controllers\VendorController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,13 +20,30 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('layouts.dashboardmaster');
-})->middleware(['admin', 'verified'])->name('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
+// Route::middleware('auth')->group(function () {
+//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+// });
 Route::middleware(['admin', 'verified'])->group(function () {
+
+    //CategoryController Resource
+    Route::resource('category', CategoryController::class);
+
     // ProfileController
     Route::get('admin/profile', [ProfileController::class, 'admin_profile'])->name('admin.profile');
     Route::get('admin/profile/setting', [ProfileController::class, 'admin_profile_setting'])->name('admin.profile.setting');
     Route::post('admin/profile/setting/edit', [ProfileController::class, 'admin_profile_setting_edit'])->name('admin.profile.setting.edit');
 });
-
 require __DIR__.'/auth.php';
+
+
+// Vendor All Routes Start
+Route::get('become/vendor', [VendorController::class, 'vendor_index'])->name('become.vendor');
+Route::post('vendor/post', [VendorController::class, 'vendor_post'])->name('vendor.post');
+Route::get('vendor/login', [VendorController::class, 'vendor_login'])->name('vendor.login');
+Route::post('vendor/login', [VendorController::class, 'vendor_login_post_form'])->name('vendor.login.post');
+Route::get('vendor/dashboard', [VendorController::class, 'vendor_dashboard'])->name('vendor.dashboard');
+
