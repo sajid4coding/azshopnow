833e2e1b9b56f406fc48f3473de8dacbe3b16000
MdRiaz-web404 <patoarimdriaz@gmail.com>
2022-11-20 00:14:39 +0600

    middleware create

--- routes/web.php
+++ routes/web.php
@@ -20,15 +20,13 @@ Route::get('/', function () {
 
 Route::get('/dashboard', function () {
     return view('layouts.dashboardmaster');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->middleware(['admin', 'verified'])->name('dashboard');
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-Route::get('admin/profile', [ProfileController::class, 'admin_profile'])->name('admin.profile');
-Route::get('admin/profile/setting', [ProfileController::class, 'admin_profile_setting'])->name('admin.profile.setting');
-Route::post('admin/profile/setting/edit', [ProfileController::class, 'admin_profile_setting_edit'])->name('admin.profile.setting.edit');
+Route::middleware(['admin', 'verified'])->group(function () {
+    // ProfileController
+    Route::get('admin/profile', [ProfileController::class, 'admin_profile'])->name('admin.profile');
+    Route::get('admin/profile/setting', [ProfileController::class, 'admin_profile_setting'])->name('admin.profile.setting');
+    Route::post('admin/profile/setting/edit', [ProfileController::class, 'admin_profile_setting_edit'])->name('admin.profile.setting.edit');
+});
 
 require __DIR__.'/auth.php';
