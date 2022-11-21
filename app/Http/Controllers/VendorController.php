974778cd397aa204ed27332a269b92cdaa846502
MdRiaz-web404 <patoarimdriaz@gmail.com>
2022-11-21 22:53:52 +0600

    Admin profile password change, vendor management

--- app/Http/Controllers/VendorController.php
+++ app/Http/Controllers/VendorController.php
@@ -22,7 +22,7 @@ class vendorController extends Controller
         $request->validate([
             '*' => 'required',
             'email' => "unique:users",
-            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()],
+            'password' => ['required', 'confirmed', Password::min(8)->mixedCase()->numbers()->symbols()],
         ]);
 
         User::insert([
