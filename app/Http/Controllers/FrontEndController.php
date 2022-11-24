539d86c5aeaaaf22be813cb9bf69bc4ad2a0b72b
Sajid <shojolislamshajid55@gmail.com>
2022-11-24 23:20:34 +0600

    Category Dynamic Fixing

--- app/Http/Controllers/FrontEndController.php
+++ app/Http/Controllers/FrontEndController.php
@@ -10,13 +10,6 @@ use Illuminate\Support\Facades\Mail;
 
 class FrontEndController extends Controller
 {
-    public function index(){
-        return view('index',[
-            'categories' => Category::where('status','published')->latest()->limit(12)->get()->shuffle(),
-            'auth_categories' => Category::where('status','published')->latest()->limit(20)->get()->shuffle()
-        ]);
-    }
-
     function contact_us_index(){
         return view('frontend.contact_us');
     }
@@ -31,5 +24,12 @@ class FrontEndController extends Controller
         Mail::to('mdshrabon.dev@gmail.com')->send(new ContactMessage($request->except('_token')));
 
         return back()->with('contact_success_message','Your message successfully we have received!');
+    }
+
+    public function index(){
+        return view('index', [
+            'categories' => Category::where('status','published')->latest()->limit(12)->get()->shuffle(),
+            'auth_categories' => Category::where('status','published')->latest()->limit(20)->get()->shuffle()
+        ]);
     }
 }
