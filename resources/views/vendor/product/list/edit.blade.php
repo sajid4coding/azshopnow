0cf6ca4546017244ae6a3a879157556d258ee82c
Sajid <shojolislamshajid55@gmail.com>
2022-12-20 21:24:42 +0600

    Review Some Edit

--- resources/views/vendor/product/list/edit.blade.php
+++ resources/views/vendor/product/list/edit.blade.php
@@ -1,6 +1,92 @@
 @extends('layouts.vendor_master')
 @section('vendor_body_content')
 <style>
+    /* gallery images upload */
+    .upload__box {
+        padding: 40px;
+    }
+    .upload__box p{
+        color: #ffffff !important;
+        margin: 0;
+        padding: 0 10px;
+    }
+    .upload__inputfile {
+        width: 0.1px;
+        height: 0.1px;
+        opacity: 0;
+        overflow: hidden;
+        position: absolute;
+        z-index: -1;
+    }
+    .upload__btn {
+        display: inline-block;
+        font-weight: 600;
+        color: #fff;
+        text-align: center;
+        min-width: 116px;
+        padding: 5px;
+        transition: all 0.3s ease;
+        cursor: pointer;
+        border: 2px solid;
+        background-color: #4045ba;
+        border-color: #4045ba;
+        border-radius: 10px;
+        line-height: 26px;
+        font-size: 14px;
+    }
+    .upload__btn:hover {
+        background-color: #FF4800;
+        border-color: #FF4800;
+        color: #4045ba;
+        transition: all 0.3s ease;
+    }
+    .upload__btn-box {
+        margin-bottom: 10px;
+    }
+    .upload__img-wrap {
+        display: flex;
+        flex-wrap: wrap;
+        margin: 0 -10px;
+    }
+    .upload__img-box {
+        width: 100px;
+        padding: 0 10px;
+        margin-bottom: 12px;
+    }
+    .upload__img-close {
+        width: 24px;
+        height: 24px;
+        border-radius: 50%;
+        background-color: rgba(0, 0, 0, 0.5);
+        position: absolute;
+        top: 10px;
+        right: 10px;
+        text-align: center;
+        line-height: 24px;
+        z-index: 1;
+        cursor: pointer;
+    }
+    .upload__img-close:after {
+        content: "✖";
+        font-size: 14px;
+        color: white;
+    }
+
+    .img-bg {
+        background-repeat: no-repeat;
+        background-position: center;
+        background-size: cover;
+        position: relative;
+        padding-bottom: 100%;
+    }
+    .product-upload-wrap form button:hover{
+        background: #ffffff !important;
+        color: #FF4800 !important;
+        border:1px solid #FF4800 !important;
+    }
+    /* gallery images upload end */
+
+    /* Tag css start */
     .tags-input-wrapper{
         background: transparent;
         padding: 10px;
@@ -30,6 +116,8 @@
         display: inline-block;
         cursor: pointer;
     }
+    /* Tag css end */
+    
 </style>
 
 
@@ -193,6 +281,92 @@
 @endsection
 @section('footer_script')
 <script>
+
+
+
+    $(document).ready(function(){
+         $('#EditcategoryDropDown').change(function(){
+             var category_id = $(this).val()
+            if(category_id){
+             $.ajaxSetup({
+                headers: {
+                    'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                }
+            });
+             $.ajax({
+                type: 'post',
+                url: '/getIDFromCategoryForEdit',
+                data: {
+                    category_id:category_id,
+                    // sub_category_id: $products->sub_category_id
+                },
+                success: function (data) {
+                    $( "#SubCategory" ).html(data);
+                }
+            });
+        }
+
+         })
+
+    })
+
+    $(document).ready(function() {
+        $('#summernote').summernote({
+            height: 200,
+        });
+    });
+
+const inputFile = document.querySelector("#picture__input");
+const pictureImage = document.querySelector(".picture__image");
+const pictureImageTxt = "Choose an thumnail";
+pictureImage.innerHTML = pictureImageTxt;
+
+inputFile.addEventListener("change", function (e) {
+  const inputTarget = e.target;
+  const file = inputTarget.files[0];
+
+  if (file) {
+    const reader = new FileReader();
+
+    reader.addEventListener("load", function (e) {
+      const readerTarget = e.target;
+
+      const img = document.createElement("img");
+      img.src = readerTarget.result;
+      img.classList.add("picture__img");
+
+      pictureImage.innerHTML = "";
+      pictureImage.appendChild(img);
+    });
+
+    reader.readAsDataURL(file);
+  } else {
+    pictureImage.innerHTML = pictureImageTxt;
+  }
+});
+
+</script>
+<script>
+    @if (session('success'))
+        const Toast = Swal.mixin({
+        toast: true,
+        position: 'top-end',
+        showConfirmButton: false,
+        timer: 3000,
+        timerProgressBar: true,
+        didOpen: (toast) => {
+            toast.addEventListener('mouseenter', Swal.stopTimer)
+            toast.addEventListener('mouseleave', Swal.resumeTimer)
+        }
+        })
+
+        Toast.fire({
+        icon: 'success',
+        title: "{{session('success')}}"
+        });
+    @endif
+</script>
+{{-- <script>
     $(document).ready(function(){
          $('#EditcategoryDropDown').change(function(){
              var category_id = $(this).val()
@@ -227,229 +401,228 @@
 
     document.getElementById('readUrl').addEventListener('change', function(){
   if (this.files[0] ) {
-    var picture = new FileReader();
-    picture.readAsDataURL(this.files[0]);
-    picture.addEventListener('load', function(event) {
-      document.getElementById('uploadedImage').setAttribute('src', event.target.result);
-      document.getElementById('uploadedImage').style.display = 'block';
-    });
-  }
-});
-
-
-
-
-
-
-const inputFile = document.querySelector("#picture__input");
-const pictureImage = document.querySelector(".picture__image");
-const pictureImageTxt = "Choose an thumnail";
-pictureImage.innerHTML = pictureImageTxt;
-
-inputFile.addEventListener("change", function (e) {
-  const inputTarget = e.target;
-  const file = inputTarget.files[0];
-
-  if (file) {
-    const reader = new FileReader();
-
-    reader.addEventListener("load", function (e) {
-      const readerTarget = e.target;
-
-      const img = document.createElement("img");
-      img.src = readerTarget.result;
-      img.classList.add("picture__img");
-
-      pictureImage.innerHTML = "";
-      pictureImage.appendChild(img);
+        var picture = new FileReader();
+        picture.readAsDataURL(this.files[0]);
+        picture.addEventListener('load', function(event) {
+        document.getElementById('uploadedImage').setAttribute('src', event.target.result);
+        document.getElementById('uploadedImage').style.display = 'block';
+        });
+    }
     });
 
-    reader.readAsDataURL(file);
-  } else {
+
+
+
+
+
+    const inputFile = document.querySelector("#picture__input");
+    const pictureImage = document.querySelector(".picture__image");
+    const pictureImageTxt = "Choose an thumnail";
     pictureImage.innerHTML = pictureImageTxt;
-  }
-});
 
+    inputFile.addEventListener("change", function (e) {
+    const inputTarget = e.target;
+    const file = inputTarget.files[0];
 
+    if (file) {
+        const reader = new FileReader();
+
+        reader.addEventListener("load", function (e) {
+        const readerTarget = e.target;
+
+        const img = document.createElement("img");
+        img.src = readerTarget.result;
+        img.classList.add("picture__img");
+
+        pictureImage.innerHTML = "";
+        pictureImage.appendChild(img);
+        });
+
+        reader.readAsDataURL(file);
+    } else {
+        pictureImage.innerHTML = pictureImageTxt;
+    }
+    });
 </script>
+
 <script>
-    (function(){
+        (function(){
 
-"use strict"
+    "use strict"
 
 
-// Plugin Constructor
-var TagsInput = function(opts){
-    this.options = Object.assign(TagsInput.defaults , opts);
-    this.init();
-}
+    // Plugin Constructor
+    var TagsInput = function(opts){
+        this.options = Object.assign(TagsInput.defaults , opts);
+        this.init();
+    }
 
-// Initialize the plugin
-TagsInput.prototype.init = function(opts){
-    this.options = opts ? Object.assign(this.options, opts) : this.options;
+    // Initialize the plugin
+    TagsInput.prototype.init = function(opts){
+        this.options = opts ? Object.assign(this.options, opts) : this.options;
 
-    if(this.initialized)
-        this.destroy();
+        if(this.initialized)
+            this.destroy();
 
-    if(!(this.orignal_input = document.getElementById(this.options.selector)) ){
-        console.error("tags-input couldn't find an element with the specified ID");
+        if(!(this.orignal_input = document.getElementById(this.options.selector)) ){
+            console.error("tags-input couldn't find an element with the specified ID");
+            return this;
+        }
+
+        this.arr = [];
+        this.wrapper = document.createElement('div');
+        this.input = document.createElement('input');
+        init(this);
+        initEvents(this);
+
+        this.initialized =  true;
         return this;
     }
 
-    this.arr = [];
-    this.wrapper = document.createElement('div');
-    this.input = document.createElement('input');
-    init(this);
-    initEvents(this);
+    // Add Tags
+    TagsInput.prototype.addTag = function(string){
 
-    this.initialized =  true;
-    return this;
-}
+        if(this.anyErrors(string))
+            return ;
 
-// Add Tags
-TagsInput.prototype.addTag = function(string){
+        this.arr.push(string);
+        var tagInput = this;
 
-    if(this.anyErrors(string))
-        return ;
+        var tag = document.createElement('span');
+        tag.className = this.options.tagClass;
+        tag.innerText = string;
 
-    this.arr.push(string);
-    var tagInput = this;
+        var closeIcon = document.createElement('a');
+        closeIcon.innerHTML = '&times;';
 
-    var tag = document.createElement('span');
-    tag.className = this.options.tagClass;
-    tag.innerText = string;
-
-    var closeIcon = document.createElement('a');
-    closeIcon.innerHTML = '&times;';
-
-    // delete the tag when icon is clicked
-    closeIcon.addEventListener('click' , function(e){
-        e.preventDefault();
-        var tag = this.parentNode;
-
-        for(var i =0 ;i < tagInput.wrapper.childNodes.length ; i++){
-            if(tagInput.wrapper.childNodes[i] == tag)
-                tagInput.deleteTag(tag , i);
-        }
-    })
-
-
-    tag.appendChild(closeIcon);
-    this.wrapper.insertBefore(tag , this.input);
-    this.orignal_input.value = this.arr.join(',');
-
-    return this;
-}
-
-// Delete Tags
-TagsInput.prototype.deleteTag = function(tag , i){
-    tag.remove();
-    this.arr.splice( i , 1);
-    this.orignal_input.value =  this.arr.join(',');
-    return this;
-}
-
-// Make sure input string have no error with the plugin
-TagsInput.prototype.anyErrors = function(string){
-    if( this.options.max != null && this.arr.length >= this.options.max ){
-        console.log('max tags limit reached');
-        return true;
-    }
-
-    if(!this.options.duplicate && this.arr.indexOf(string) != -1 ){
-        console.log('duplicate found " '+string+' " ')
-        return true;
-    }
-
-    return false;
-}
-
-// Add tags programmatically
-TagsInput.prototype.addData = function(array){
-    var plugin = this;
-
-    array.forEach(function(string){
-        plugin.addTag(string);
-    })
-    return this;
-}
-
-// Get the Input String
-TagsInput.prototype.getInputString = function(){
-    return this.arr.join(',');
-}
-
-
-// destroy the plugin
-TagsInput.prototype.destroy = function(){
-    this.orignal_input.removeAttribute('hidden');
-
-    delete this.orignal_input;
-    var self = this;
-
-    Object.keys(this).forEach(function(key){
-        if(self[key] instanceof HTMLElement)
-            self[key].remove();
-
-        if(key != 'options')
-            delete self[key];
-    });
-
-    this.initialized = false;
-}
-
-// Private function to initialize the tag input plugin
-function init(tags){
-    tags.wrapper.append(tags.input);
-    tags.wrapper.classList.add(tags.options.wrapperClass);
-    tags.orignal_input.setAttribute('hidden' , 'true');
-    tags.orignal_input.parentNode.insertBefore(tags.wrapper , tags.orignal_input);
-}
-
-// initialize the Events
-function initEvents(tags){
-    tags.wrapper.addEventListener('click' ,function(){
-        tags.input.focus();
-    });
-
-
-    tags.input.addEventListener('keydown' , function(e){
-        var str = tags.input.value.trim();
-
-        if( !!(~[9 , 13 , 188].indexOf( e.keyCode ))  )
-        {
+        // delete the tag when icon is clicked
+        closeIcon.addEventListener('click' , function(e){
             e.preventDefault();
-            tags.input.value = "";
-            if(str != "")
-                tags.addTag(str);
+            var tag = this.parentNode;
+
+            for(var i =0 ;i < tagInput.wrapper.childNodes.length ; i++){
+                if(tagInput.wrapper.childNodes[i] == tag)
+                    tagInput.deleteTag(tag , i);
+            }
+        })
+
+
+        tag.appendChild(closeIcon);
+        this.wrapper.insertBefore(tag , this.input);
+        this.orignal_input.value = this.arr.join(',');
+
+        return this;
+    }
+
+    // Delete Tags
+    TagsInput.prototype.deleteTag = function(tag , i){
+        tag.remove();
+        this.arr.splice( i , 1);
+        this.orignal_input.value =  this.arr.join(',');
+        return this;
+    }
+
+    // Make sure input string have no error with the plugin
+    TagsInput.prototype.anyErrors = function(string){
+        if( this.options.max != null && this.arr.length >= this.options.max ){
+            console.log('max tags limit reached');
+            return true;
         }
 
-    });
-}
+        if(!this.options.duplicate && this.arr.indexOf(string) != -1 ){
+            console.log('duplicate found " '+string+' " ')
+            return true;
+        }
+
+        return false;
+    }
+
+    // Add tags programmatically
+    TagsInput.prototype.addData = function(array){
+        var plugin = this;
+
+        array.forEach(function(string){
+            plugin.addTag(string);
+        })
+        return this;
+    }
+
+    // Get the Input String
+    TagsInput.prototype.getInputString = function(){
+        return this.arr.join(',');
+    }
 
 
-// Set All the Default Values
-TagsInput.defaults = {
-    selector : '',
-    wrapperClass : 'tags-input-wrapper',
-    tagClass : 'tag',
-    max : null,
-    duplicate: false
-}
+    // destroy the plugin
+    TagsInput.prototype.destroy = function(){
+        this.orignal_input.removeAttribute('hidden');
 
-window.TagsInput = TagsInput;
+        delete this.orignal_input;
+        var self = this;
 
-})();
+        Object.keys(this).forEach(function(key){
+            if(self[key] instanceof HTMLElement)
+                self[key].remove();
 
-var tagInput1 = new TagsInput({
-        selector: 'tag-input1',
-        duplicate : false,
-        max : 10
-    });
-    // tagInput1.addData(['PHP' , 'JavaScript' , 'CSS'])
-    tagInput1.addData([ ])
+            if(key != 'options')
+                delete self[key];
+        });
 
-</script>
+        this.initialized = false;
+    }
+
+    // Private function to initialize the tag input plugin
+    function init(tags){
+        tags.wrapper.append(tags.input);
+        tags.wrapper.classList.add(tags.options.wrapperClass);
+        tags.orignal_input.setAttribute('hidden' , 'true');
+        tags.orignal_input.parentNode.insertBefore(tags.wrapper , tags.orignal_input);
+    }
+
+    // initialize the Events
+    function initEvents(tags){
+        tags.wrapper.addEventListener('click' ,function(){
+            tags.input.focus();
+        });
+
+
+        tags.input.addEventListener('keydown' , function(e){
+            var str = tags.input.value.trim();
+
+            if( !!(~[9 , 13 , 188].indexOf( e.keyCode ))  )
+            {
+                e.preventDefault();
+                tags.input.value = "";
+                if(str != "")
+                    tags.addTag(str);
+            }
+
+        });
+    }
+
+
+    // Set All the Default Values
+    TagsInput.defaults = {
+        selector : '',
+        wrapperClass : 'tags-input-wrapper',
+        tagClass : 'tag',
+        max : null,
+        duplicate: false
+    }
+
+    window.TagsInput = TagsInput;
+
+    })();
+
+    var tagInput1 = new TagsInput({
+            selector: 'tag-input1',
+            duplicate : false,
+            max : 10
+        });
+        // tagInput1.addData(['PHP' , 'JavaScript' , 'CSS'])
+        tagInput1.addData([ ])
+
+</script> --}}
 
 
 @endsection
